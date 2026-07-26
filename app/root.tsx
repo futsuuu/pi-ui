ac373ddb45ce6ac3ad0e57633ce223d568dade76
futsuuu <futsuuu123@gmail.com>
2026-07-26 11:08:17 +0900

    feat: use Noto Sans JP and JetBrains Mono

--- app/root.tsx
+++ app/root.tsx
@@ -14,15 +14,13 @@ import "./app.css";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
+  { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
+  { rel: "preconnect", href: "https://rsms.me/" },
   {
     rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+    href: "https://fonts.googleapis.com/css2?family=JetBrains+Mono&display=swap",
   },
+  { rel: "stylesheet", href: "https://rsms.me/inter/inter.css" },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
